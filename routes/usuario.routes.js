1f44d3d5685bcd911f0430b99a941dd46a42479c
Perillos <valero.ce@gmail.com>
2022-07-03 11:59:38 +0200

    get and post

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -5,9 +5,12 @@ const router = express.Router();
 
 
 router.get('/', function(req, res) {
-    res.json({msg: 'Hola'})
+    res.json({msg: 'Respuesta de tipo Get'})
 });
 
+router.post('/', function(req, res) {
+    res.json({msg: 'Respuesta de tipo Post'})
+})
 
 
 export default router
