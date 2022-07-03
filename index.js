ad4d5109f5d038b8594438512606699d787f37a4
Perillos <valero.ce@gmail.com>
2022-07-03 16:10:24 +0200

    render con pug

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuario.routes.js'
 
+// Crar la app
 const app = express()
 
-app.use('/', usuarioRoutes)
+// Habiliar pug
+app.set('view engine', 'pug')
+// La carpeta donde estan las vistas
+app.set('views', './views')
 
+// Routing
+app.use('/auth', usuarioRoutes)
+
+
+// Define el puerto
 const port = 3000;
 
+// Arranque del proyecto
 app.listen(port, () => {
     console.log(`El Servidor esta funcionando en el puero ${port}`);
 })
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -4,21 +4,10 @@ import express from 'express';
 const router = express.Router();
 
 
-// router.get('/', function(req, res) {
-//     res.json({msg: 'Respuesta de tipo Get'})
-// });
+router.get('/login', (req, res) => {
+    res.render('auth/login')
+});
 
-// router.post('/', function(req, res) {
-//     res.json({msg: 'Respuesta de tipo Post'})
-// })
-
-router.route('/')
-    .get(function(req, res) {
-        res.json({msg: 'Respuesta de tipo Get'})
-    })
-    .post(function(req, res) {
-        res.json({msg: 'Respuesta de tipo Post'})
-    })
 
 
 export default router
