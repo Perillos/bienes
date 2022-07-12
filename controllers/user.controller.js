9021456c57fe838f5ebd9c5c8170bcfd540b855b
Perillos <valero.ce@gmail.com>
2022-07-12 10:46:51 +0200

    show errors validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,11 +15,26 @@ const formRegister = (req, res) => {
 
 const postRegister = async (req, res) => {
     // Validación
-    await check('name').notEmpty().run(req)
+    await check('name').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
+    await check('email').isEmail().withMessage('Escribe un Email válido').run(req)
+    await check('password').isLength({ min: 6 }).withMessage('La contraseña tien que tener 6 caracteres con letras y números').run(req)
+    await check('rep_password').equals(req.body.password).withMessage('Las contraseña no son iguales').run(req)
 
     let result = validationResult(req)
 
-    res.json(result)
+    // Comprobar que la validacióna ha pasado para llevar los datos a la BBDD
+    if (!result.isEmpty()) { // Hay errores, resutl no esta vacio por lo que hay errores
+        return res.render('auth/register', {
+            page: 'Crear Cuenta',
+            errors: result.array(),
+            // Cuando un usuario pone algún dato mas se borra, con esto se quedan los datos anteriormente insertados para que no los tenga que repetir.
+            user: {
+                name: req.body.name,
+                email: req.body.email
+            }
+        })
+        
+    }
 
     const user = await User.create(req.body)
     res.json(user)
